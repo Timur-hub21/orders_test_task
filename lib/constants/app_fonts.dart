6c1dafcebb7fc52aacaf32cc5425b13a2203d500
Timur <e@MacBook-Pro-pkva.local>
2024-01-29 09:54:49 +0300

    Added application fonts

---
+++ lib/constants/app_fonts.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+final class AppFonts {
+  const AppFonts._();
+
+  /// Not used font
+  static const TextStyle header24PtBold = TextStyle();
+
+  static const TextStyle header20PtSemiBold = TextStyle(
+    fontFamily: 'Roboto',
+    fontWeight: FontWeight.w600,
+    fontSize: 20,
+  );
+
+  static const TextStyle header16PtBold = TextStyle(
+    fontFamily: 'Roboto',
+    fontWeight: FontWeight.w700,
+    fontSize: 16,
+  );
+
+  /// Not used font
+  static const TextStyle header14PtBold = TextStyle();
+
+  static const TextStyle header14PtSemiBold = TextStyle(
+    fontFamily: 'Roboto',
+    fontWeight: FontWeight.w600,
+    fontSize: 14,
+  );
+
+  /// Not used font
+  static const TextStyle text32PtBold = TextStyle();
+
+  /// Not used font
+  static const TextStyle text20PtRegular = TextStyle();
+
+  static const TextStyle text16PtMedium = TextStyle(
+    fontFamily: 'Roboto',
+    fontWeight: FontWeight.w500,
+    fontSize: 16,
+  );
+
+  /// Not used font
+  static const TextStyle text16PtRegular = TextStyle();
+
+  static const TextStyle text14PtRegular = TextStyle(
+    fontFamily: 'Roboto',
+    fontWeight: FontWeight.w400,
+    fontSize: 14,
+  );
+
+  /// Not used font
+  static const TextStyle text12PtSemiBold = TextStyle();
+
+  /// Not used font
+  static const TextStyle text12PtRegular = TextStyle();
+
+  /// Not used font
+  static const TextStyle linkText15PtRegular = TextStyle();
+}
